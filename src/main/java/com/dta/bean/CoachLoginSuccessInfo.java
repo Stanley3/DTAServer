6dd49afd142db9e8c55215344ad7ad819c7a75a2
user.name <myfu90@gmail.com>
2015-06-30 23:57:58 +0800

    coach login

---
+++ src/main/java/com/dta/bean/CoachLoginSuccessInfo.java
@@ -0,0 +1,19 @@
+package com.dta.bean;
+
+public class CoachLoginSuccessInfo {
+	private Integer coach_id;
+	private Integer code;
+	
+	public Integer getCoach_id() {
+		return coach_id;
+	}
+	public void setCoach_id(Integer coach_id) {
+		this.coach_id = coach_id;
+	}
+	public Integer getCode() {
+		return code;
+	}
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+}
